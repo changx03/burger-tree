2ff19afdb1290f31dc722729cab854d4fb748e09
changx03 <luke.x.chang@gmail.com>
2018-03-07 03:37:24 +1300

    updated order page

--- src/containers/Order.js
+++ src/containers/Order.js
@@ -1,11 +1,33 @@
 import React from 'react';
 import styleClasses from './Order.css';
 
-const order = props => (
-  <div className={styleClasses.Order}>
-    <p>Ingredients: Salad</p>
-    <p>Price: <strong>6</strong></p>
-  </div>
-);
+const order = props => {
+  console.log(props);
+  const ingredientsStr = Object.keys(props.ingredients)
+    .filter(key => props.ingredients[key] !== 0)
+    .map(key => (
+      <span
+        key={key} 
+        style={{ 
+          textTransform: 'capitalize',
+          display: 'inline-block',
+          margin: '0 8px',
+          border: '1px solid #ccc',
+          padding: 5,
+          borderRadius: 4,
+        }}
+      >
+        {`${key} (${props.ingredients[key]})`}
+      </span>
+      )
+    );
+
+  return (
+    <div className={styleClasses.Order}>
+      <p>Ingredients: {ingredientsStr}</p>
+      <p>Price: <strong>${props.price.toFixed(2)}</strong></p>
+    </div>
+  );
+}
 
 export default order;
--- src/containers/Orders.js
+++ src/containers/Orders.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react';
+import axios from '../axios-orders';
 import Order from './Order';
+import errorHandler from './errorHandler';
+import Spinner from '../components/UI/Spinner';
 
-export default class Orders extends Component {
+class Orders extends Component {
+  state = {
+    orders: [],
+    loading: true,
+  };
+
+  componentDidMount() {
+    axios.get('/orders.json').then(res => {
+      const orders = Object.keys(res.data).map(key => ({...res.data[key], id: key}));
+      console.log("[Orders]", orders);
+      this.setState({ loading: false, orders: orders });
+    }).catch(err => {
+      this.setState({ loading: false });
+    });
+  }
+
   render() {
     return (
-      <div>
-        <Order />
-        <Order />
-      </div>
+      <React.Fragment>
+        {this.state.loading ? <Spinner /> 
+          : this.state.orders.map(order => <Order key={order.id} {...order} />)
+        }
+      </React.Fragment>
     );
   }
 }
+
+export default errorHandler(Orders, axios);
