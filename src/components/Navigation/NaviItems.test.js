67747ba22d374f0b350d8907533e8cdf83fde09a
Luke Chang <luke@csel.co.nz>
2018-04-09 13:42:35 +1200

    added test

--- src/components/Navigation/NaviItems.test.js
+++ src/components/Navigation/NaviItems.test.js
@@ -7,8 +7,22 @@ import NaviItem from './NaviItem';
 configure({ adapter: new Adapter() });
 
 describe('<NavigationItems />', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<NaviItems />);
+  });
+
   it('Should render 2 <NaviItem /> components if not authenticated', () => {
-    const wrapper = shallow(<NaviItems />);
     expect(wrapper.find(NaviItem)).toHaveLength(2);
+  });
+
+  it('Should render 3 <NaviItem /> components if authenticated', () => {
+    wrapper.setProps({ isAuth: true });
+    expect(wrapper.find(NaviItem)).toHaveLength(3);
+  });
+
+  it('Should has Logout component if authenticated', () => {
+    wrapper.setProps({ isAuth: true });
+    expect(wrapper.contains(<NaviItem link="/logout">Logout</NaviItem>)).toEqual(true);
   });
 });
